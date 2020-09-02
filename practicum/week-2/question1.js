c5cf61b98f2b8ef8e4a8c83909fc6ca2e20298af
RighteousRiley <rcsimpso@gmail.com>
2020-09-02 11:19:56 -0700

    Solution for q1

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -10,5 +10,13 @@ is at least twice as much as every other number
 */
 
 var largestNumberIsAtLeastTwice = function(input) {
-   
+   var max = Math.max.apply(null, input);
+   for(let i=0; i<input.length;i++) {
+           var currNum = input[i];
+           if(currNum * 2 > max && currNum != max) {
+                return false;
+           }
+   }
+   return true;
 };
+console.log(largestNumberIsAtLeastTwice([3,2,21,50]));
