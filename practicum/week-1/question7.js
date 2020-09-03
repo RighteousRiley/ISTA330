16233cc2fa95547a7f9a670d458ef601f5fca0ae
RighteousRiley <rcsimpso@gmail.com>
2020-09-03 12:49:14 -0700

    q7

--- practicum/week-1/question7.js
+++ practicum/week-1/question7.js
@@ -10,6 +10,43 @@ output: 4
        whatever other pairs we choose the sum of the minimums will not exceed 4.
 */
 
+// all possible pairs
 var maxSumOfMins = function(input) {
-
+       var possiblePairs = [];
+       var maxMin = 0;
+       for(let i=0;i<input.length;i++) {
+              for(let j=0;j<input.length;j++) {
+                     if(input[i] !== input[j]) {
+                            possiblePairs.push({x:input[i], y:input[j]});
+                     }
+              }
+       }
+       console.log(possiblePairs);
+       for(let i=0;i<possiblePairs.length;i++) {
+              var firstPair = possiblePairs[i];
+              for(let j=1;j<possiblePairs.length;j++) {
+                     var secondPair = possiblePairs[j];
+                     if (firstPair.x === secondPair.x || firstPair.x === secondPair.y) {
+                            continue;
+                     }
+                     if (firstPair.y === secondPair.x || firstPair.y === secondPair.y) {
+                            continue;
+                     }
+                     if(firstPair.x < firstPair.y) {
+                            firstPairMin = firstPair.x;
+                     } else {
+                            firstPairMin = firstPair.y
+                     } if(secondPair.x < secondPair.y) {
+                            secondPairMin = secondPair.x;
+                     } else {
+                            secondPairMin = secondPair.y
+                     }
+                     var minSum = firstPairMin + secondPairMin;
+                     if(minSum > maxMin) {
+                            maxMin = minSum;
+                     }
+              }
+       }
+       return maxMin;
 };
+console.log(maxSumOfMins([4,3,8,0]));
