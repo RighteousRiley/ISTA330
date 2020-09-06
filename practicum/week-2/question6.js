aff2cc99f38bccdb7c0e181dfb996558e64b4be8
RighteousRiley <rcsimpso@gmail.com>
2020-09-06 15:52:22 -0700

    q6 completed

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -12,5 +12,19 @@ output: 2
 */
 
 var m_element = function(input) {
-
+    var mNum = Math.floor(input.length / 2);
+    for(let i=0;i<input.length;i++) {
+        var currNum = input[i];
+        var currNumCount = 0;
+        for(let j=i+1;j<input.length;j++) {
+            if(currNum === input[j]) {
+                currNumCount += 1;
+            }
+        }
+        if (currNumCount >= mNum) {
+            return currNum;
+        }
+    }
+    return -1;
 };
+console.log(m_element([1,2,3,4,5,6,7,8]));
