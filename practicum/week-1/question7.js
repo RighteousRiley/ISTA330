b50d6ce8e418c3593acc8b276736c7f37da5937d
RighteousRiley <rcsimpso@gmail.com>
2020-09-06 14:38:07 -0700

    Fixed q7 bugs

--- practicum/week-1/question7.js
+++ practicum/week-1/question7.js
@@ -3,7 +3,7 @@ Given an array of 2n numbers, group the numbers into n pairs of integer,
 such that the sum of the minimum of each pair is the largest.
 
 Example: 
-input: [4, 3, 8, 0]
+input: [4, 3, 8, 0] 8,7,6,5,4,3,2,1
 output: 4
        because we can make the following pairs: (4, 8) and (3, 0) and 
        min(4,8) + min(3,0) = 4
@@ -12,6 +12,30 @@ output: 4
 
 // all possible pairs
 var maxSumOfMins = function(input) {
+       var sortedInput = bubbleSort(input);
+       console.log(sortedInput);
+       var minSum = 0;
+       for(let i=0; i<sortedInput.length;i+=2) {
+              minSum += sortedInput[i];
+       }
+       return minSum;
+};
+
+function bubbleSort(input) {
+       var length = input.length;
+       for (var i = length-1; i >= 0; i--){
+              for(var j = 1; j <= i; j++){
+                     if(input[j-1] > input[j]){
+                     var item = input[j-1];
+                     input[j-1] = input[j];
+                     input[j] = item;
+              }
+       }
+    }
+    return input;
+}
+
+function findPairs(input) {
        var possiblePairs = [];
        var maxMin = 0;
        for(let i=0;i<input.length;i++) {
@@ -21,32 +45,6 @@ var maxSumOfMins = function(input) {
                      }
               }
        }
-       console.log(possiblePairs);
-       for(let i=0;i<possiblePairs.length;i++) {
-              var firstPair = possiblePairs[i];
-              for(let j=1;j<possiblePairs.length;j++) {
-                     var secondPair = possiblePairs[j];
-                     if (firstPair.x === secondPair.x || firstPair.x === secondPair.y) {
-                            continue;
-                     }
-                     if (firstPair.y === secondPair.x || firstPair.y === secondPair.y) {
-                            continue;
-                     }
-                     if(firstPair.x < firstPair.y) {
-                            firstPairMin = firstPair.x;
-                     } else {
-                            firstPairMin = firstPair.y
-                     } if(secondPair.x < secondPair.y) {
-                            secondPairMin = secondPair.x;
-                     } else {
-                            secondPairMin = secondPair.y
-                     }
-                     var minSum = firstPairMin + secondPairMin;
-                     if(minSum > maxMin) {
-                            maxMin = minSum;
-                     }
-              }
-       }
-       return maxMin;
-};
-console.log(maxSumOfMins([4,3,8,0]));
+       return possiblePairs;
+}
+console.log(maxSumOfMins([1,3,4,2,6,8,5,7]));
