eb5dbb276e51cdffb6a479be32149d6d3a5fd5e3
RighteousRiley <rcsimpso@gmail.com>
2020-09-06 16:04:56 -0700

    q7 test

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -12,5 +12,37 @@ output: [[1,4], [2,5], [3,6]]
 */
 
 var transpose = function(M) {
-
+    var rows = findRows(M);
+    var cols = findCols(M);
+    return makeNewMatrix(rows, cols);
 };
+
+function findRows(M) {
+    var rows = [];
+    for(let i=0;i<M.length;i++) {
+        rows.push(M[i]);
+    }
+    return rows;
+}
+
+function findCols(M) {
+    var cols = [];
+    for(let i=0;i<M[0].length;i++) {
+        var currCol = [];
+        for(let j=0;j<M.length;j++) {
+            var currRow = j;
+            currCol.push(M[currRow][i]);
+        }
+        cols.push(currCol);
+    }
+    return cols;
+}
+
+function makeNewMatrix(rows, cols) {
+    var matrix = [];
+    for(let i=0;i<cols.length;i++) {
+        matrix.push(cols[i]);
+    }
+    return matrix;
+}
+console.log(transpose([[1,4,2],[5,3,6]]));
