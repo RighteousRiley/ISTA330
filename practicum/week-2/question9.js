037be0fd0d6acad9428eea9672de4c200aafa5eb
RighteousRiley <rcsimpso@gmail.com>
2020-09-06 19:23:33 -0700

    Completed q9

--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -13,5 +13,41 @@ so there are two groups with the largest size.
 */
 
 var largestGroupsCount = function(n) {
-
+    var groups = makeGroups(n);
+    var highestSumGroups = findHighestSum(groups);
+    return highestSumGroups;
 };
+
+function makeGroups(n) {
+    var groups = [];
+    var i = 1;
+    for(i;i<10;i++) {
+        let arr = [];    
+        arr.push(i);
+        groups.push(arr);    
+    }        
+    while(i<=n) {
+        var currGroupIndex = i % 10;
+        groups[currGroupIndex].push(i);
+        i++;
+    }
+    return groups;
+}
+
+function findHighestSum(groups) {
+    var highestGroupLength = 0;
+    var numHighGroups = 0;
+    for(let currGroup of groups) {
+        if(currGroup.length > highestGroupLength) {
+            highestGroupLength = currGroup.length;
+            numHighGroups = 1;
+        } else if(currGroup.length === highestGroupLength) {
+            numHighGroups += 1;
+        } else {
+            continue;
+        }
+    }
+    return numHighGroups;
+}
+
+console.log(largestGroupsCount(11));
