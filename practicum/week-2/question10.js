b7929adb42a8fe193bd777a02c3f3be914bb36cb
RighteousRiley <39767460+RighteousRiley@users.noreply.github.com>
2020-09-07 13:43:20 -0700

    finished q10

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -16,5 +16,61 @@ output: [[-5, -4], [23, 24]]
 */
 
 var minPairs = function(input) {
-
+    var minDiff = findMinDiff(input);
+    return findPossiblePairs(input, minDiff);
 };
+
+function findPossiblePairs(input,minDiff) {
+    var pairs = [];
+    for(let i=0;i<input.length;i++) {
+        var firstNum = input[i];
+        for(let j=1;j<input.length;j++) {
+            var secondNum = input[j];
+            var diff = Math.abs(secondNum - firstNum);
+            if(diff === minDiff) {
+                var sortedPair = bubbleSort([firstNum, secondNum]);
+                if(!hasPairs(pairs, sortedPair)) {
+                    pairs.push(sortedPair);
+                }
+            }
+        }
+    }
+    return pairs;
+}
+function hasPairs(pairList, toCheck) {
+    for(let currPair of pairList) {
+        if(currPair[0] === toCheck[0] && currPair[1] === toCheck[1]) {
+            return true;
+        }
+    }
+    return false;
+}
+function findMinDiff(input) {
+    var minDiff = 10000;
+    for(let i=0;i<input.length;i++) {
+        var firstNum = input[i];
+        for(let j=1;j<input.length;j++) {
+            var secondNum = input[j];
+            var diff = Math.abs(secondNum - firstNum);
+            if(firstNum < secondNum &&  diff <= minDiff) {
+                minDiff = diff;
+            }
+        }
+    }
+    return minDiff;
+}
+function bubbleSort(input) {
+    var length = input.length;
+    for (var i = length-1; i >= 0; i--){
+           for(var j = 1; j <= i; j++){
+                  if(input[j-1] > input[j]){
+                  var item = input[j-1];
+                  input[j-1] = input[j];
+                  input[j] = item;
+           }
+    }
+ }
+ return input;
+}
+
+console.log(minPairs([17,-5,-1,24,29,54,1]));
