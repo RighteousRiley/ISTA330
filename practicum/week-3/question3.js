ea0f8ca6fdb21b25efef1319ebfde3985cf6aec3
RighteousRiley <rcsimpso@gmail.com>
2020-09-17 14:53:16 -0700

    Finished q3

--- practicum/week-3/question3.js
+++ practicum/week-3/question3.js
@@ -5,9 +5,18 @@ otherwise return false.
 
 Example:
 input: input = [1,0,0,0,1,0,0,1], k = 2
+"1 0 0 0 1 0 0 1"
 output: true
 */
 
 var kStepAway = function(input, k) {
-
+    splitList = input.join("").split("1");
+    console.log(splitList);
+    for(let substr of splitList) {
+        if(substr.length < k && substr != '') {
+            return false;
+        }
+    }
+    return true;
 };
+console.log(kStepAway([1,0,0,1,0,1], 2));
