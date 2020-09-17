0cbee7766950e07262acc65bb6d5f10e5893f192
RighteousRiley <rcsimpso@gmail.com>
2020-09-17 14:21:17 -0700

    Completed question 2

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -9,8 +9,27 @@ Example: input: [19,3,2,10,8,2,3,5]
          output: [2,3]
                 
           */
-
+function bubbleSort(input) {
+    var length = input.length;
+    for (var i = length-1; i >= 0; i--){
+            for(var j = 1; j <= i; j++){
+                    if(input[j-1] > input[j]){
+                    var item = input[j-1];
+                    input[j-1] = input[j];
+                    input[j] = item;
+                }
+            }
+    }
+    return input;
+}
  var findDuplicates = function(input) {
-     
-    
+    bubbleSort(input);
+    dups = [];
+    for(i=0;i<input.length-1;i++) {
+        if((input[i] in input.slice(i+1)) && !(dups.includes(input[i]))) {
+            dups.push(input[i]);
+        }
+    }
+    return dups;
  };
+console.log(findDuplicates([-17,3,1,5,-9,1,10]));
