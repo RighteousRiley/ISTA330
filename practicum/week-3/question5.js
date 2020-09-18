d150779c3eac8c8eea4075a0cfd3826db5324bdd
RighteousRiley <rcsimpso@gmail.com>
2020-09-17 19:53:53 -0700

    completed q5

--- practicum/week-3/question5.js
+++ practicum/week-3/question5.js
@@ -10,6 +10,16 @@ output: [24,12,8,6]
 */
 
 var productOfOthers = function(input) {
-
+    var output = [];
+    for(let i=0;i<input.length;i++) {
+        var product = 1;
+        for(let j of input) {
+            if(input[i] != j) {
+                product = product * j;
+            }
+        }
+        output[i] = product;
+    }
+    return output;
 };
-
+console.log(productOfOthers([7,10,5]))
